71c7da9fa400bfd255fc9cec5c3538816efd04c6
260439m <1006245347@qq.com>
2024-07-29 09:55:17 +0800

    fix[更新]: v1.1.4

--- app/src/main/java/com/lyentech/npuse/NUApp.java
+++ app/src/main/java/com/lyentech/npuse/NUApp.java
@@ -4,11 +4,22 @@ import android.app.Application;
 
 import com.lyentech.sdk.GreeNp;
 
+import java.util.ArrayList;
+
 public class NUApp extends Application {
    @Override
    public void onCreate() {
       super.onCreate();
-      GreeNp.init(this,"02EBA73C32A2AF47552FA411135E2CBA",true);
+//      GreeNp.initSdk(this,"02EBA73C32A2AF47552FA411135E2CBA",true);
 
+
+      String appKey = "02EBA73C32A2AF47552FA411135E2CBA";
+      ArrayList<String> appKeyArr = new ArrayList<>();
+      appKeyArr.add("0D511ECC570BAA9BDAE6075B9F0336FC");
+      ArrayList<String> eventArr = new ArrayList<>();
+      eventArr.add("first_data_download");
+      eventArr.add("second_data_download");
+      //多项目统计
+      GreeNp.initSdk(this, appKey, appKeyArr, eventArr,true);
    }
 }
